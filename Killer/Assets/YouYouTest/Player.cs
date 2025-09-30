8a6d445286be0d0c443bf4a3c84ebd66d4ce784c
LAPTOP-036TD6GS\fun <1023571809@qq.com>
2025-09-30 15:02:09 +0800

    关卡构建

--- Killer/Assets/YouYouTest/Player.cs
+++ Killer/Assets/YouYouTest/Player.cs
@@ -29,6 +29,10 @@ public class Player : MonoBehaviour, ICanBeHit
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.R)) 
+        {
+            Respawn();
+        }
 
     }
 
