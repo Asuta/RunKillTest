90f2b890c5b67bc5ff6c30d79977470bdbdb0ec3
LAPTOP-036TD6GS\fun <1023571809@qq.com>
2025-09-30 13:40:34 +0800

    玩家死亡复活

--- Killer/Assets/YouYouTest/Player.cs
+++ Killer/Assets/YouYouTest/Player.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour,ICanBeHit
+public class Player : MonoBehaviour, ICanBeHit
 {
     public void TakeDamage(int damage)
     {
-        Debug.Log($"Player took {damage} damage");   
+        Debug.Log($"Player took {damage} damage");
         health -= damage;
         if (health <= 0)
         {
@@ -12,24 +12,44 @@ public class Player : MonoBehaviour,ICanBeHit
         }
     }
 
-    public int health = 100;
+    public int health = 1;
+    public GameObject deathRed;
+    private Vector3 initialPosition; // 存储初始位置
+    private Vector3 initialRotation; // 存储初始旋转
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // 记录游戏开始时的初始位置
+        initialPosition = transform.position;
+        initialRotation = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void Die()
     {
         Debug.Log("Player died!");
-        // // 在这里添加玩家死亡的逻辑，比如播放动画、掉落物品等
-        // Destroy(gameObject);
+        // 显示红色面板
+        deathRed.SetActive(true);
+        // 5秒后执行复活
+        Invoke(nameof(Respawn), 2.5f); 
+    }
+
+    private void Respawn()
+    {
+        // 重置玩家到初始位置
+        transform.position = initialPosition;
+        transform.rotation = Quaternion.Euler(initialRotation);
+
+        // 隐藏红色面板
+        deathRed.SetActive(false);
+        // 恢复生命值
+        health = 1;
+        Debug.Log("Player respawned at initial position!");
     }
 }
