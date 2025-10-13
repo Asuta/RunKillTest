71efe24e80df055715e6777f3c04d3c8effd9f8b
LAPTOP-036TD6GS\fun <1023571809@qq.com>
2025-10-13 16:33:00 +0800

    检查点初步实现

---
+++ Killer/Assets/YouYouTest/Scripts/CheckPoint/CheckPoint.cs
@@ -0,0 +1,88 @@
+using System;
+using UnityEngine;
+using VInspector;
+
+public class CheckPoint : MonoBehaviour
+{
+    // 定义三种状态
+    public enum CheckPointState
+    {
+        Inactive,      // 未激活
+        Activating,    // 正在激活
+        Activated      // 激活过了
+    }
+
+    // 状态对应的颜色
+    [SerializeField] private Color inactiveColor = Color.gray;
+    [SerializeField] private Color activatingColor = Color.yellow;
+    [SerializeField] private Color activatedColor = Color.green;
+
+    // 当前状态
+    private CheckPointState currentState = CheckPointState.Inactive;
+    
+    // 材质引用
+    private Material material;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 获取材质组件
+        material = GetComponent<Renderer>().material;
+        // 初始化颜色
+        UpdateMaterialColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 更新材质颜色
+    private void UpdateMaterialColor()
+    {
+        switch (currentState)
+        {
+            case CheckPointState.Inactive:
+                material.color = inactiveColor;
+                break;
+            case CheckPointState.Activating:
+                material.color = activatingColor;
+                break;
+            case CheckPointState.Activated:
+                material.color = activatedColor;
+                break;
+        }
+    }
+
+    // 设置状态
+    public void SetState(CheckPointState newState)
+    {
+        currentState = newState;
+        UpdateMaterialColor();
+    }
+
+    // 获取当前状态
+    public CheckPointState GetCurrentState()
+    {
+        return currentState;
+    }
+
+    [Button("Set Activating")]
+    public void TestSetState()
+    {
+        SetState(CheckPointState.Activating);
+    }
+
+    [Button("Set Activated")]
+    public void TestSetState2()
+    {
+        SetState(CheckPointState.Activated);
+    }
+
+    [Button("Set Inactive")]
+    public void TestSetState3()
+    {
+        SetState(CheckPointState.Inactive);
+    }
+}
