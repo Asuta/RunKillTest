deeda8b1599456c7a7201df19c3099bf86e5846c
LAPTOP-036TD6GS\fun <1023571809@qq.com>
2025-10-14 11:44:48 +0800

    VR砍人实现

--- Killer/Assets/YouYouTest/Scripts/VRPlayer/VRBlade.cs
+++ Killer/Assets/YouYouTest/Scripts/VRPlayer/VRBlade.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class VRBlade : MonoBehaviour
 {
+    #region 公共变量
     public MeshRenderer bladeMeshRenderer;
     public float speedThreshold = 2.0f; // 速度阈值，大于此值时变红
-    
+    public GameObject playerHit;
+    public GameObject playerDefense;
+    #endregion
+
+    #region 私有变量
     private Vector3 previousPosition;
     private bool isRed = false;
-    public GameObject PlayerHit;
-    public GameObject PlayerDefense;
+    #endregion
 
+    #region Unity生命周期方法
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +25,33 @@ public class VRBlade : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        UpdateBladeColorBasedOnSpeed();
+    }
+
+    // 当刀片碰撞到其他物体时触发
+    private void OnTriggerEnter(Collider other)
+    {
+        // 记录碰撞物体的名称
+        Debug.Log($"刀片碰到了: {other.gameObject.name}");
+        
+        // 检查碰撞物体是否为敌人并且有ICanBeHit接口
+        if (other.CompareTag("Enemy"))
+        {
+            ICanBeHit canBeHit = other.attachedRigidbody.GetComponent<ICanBeHit>();
+            if (canBeHit != null)
+            {
+                // 调用伤害方法，这里假设造成50点伤害
+                canBeHit.TakeDamage(50);
+                Debug.Log($"对敌人造成50点伤害");
+            }
+        }
+    }
+    #endregion
+
+    #region 刀片颜色管理
+    
+    private void UpdateBladeColorBasedOnSpeed()
     {
         // 计算当前速度
         Vector3 currentPosition = transform.position;
@@ -45,14 +77,15 @@ public class VRBlade : MonoBehaviour
     public void SetBladeRed()
     {
         bladeMeshRenderer.material.color = Color.red;
-        PlayerHit.SetActive(true);
-        PlayerDefense.SetActive(false);
+        playerHit.SetActive(true);
+        playerDefense.SetActive(false);
     }
     
     public void SetBladeBlack()
     {
         bladeMeshRenderer.material.color = Color.black;
-        PlayerHit.SetActive(false);
-        PlayerDefense.SetActive(true);
+        playerHit.SetActive(false);
+        playerDefense.SetActive(true);
     }
+    #endregion
 }
