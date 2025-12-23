0d17c0d2f03249ec8e8943f3423fae2833a7a3d3
LAPTOP-036TD6GS\fun <1023571809@qq.com>
2025-12-23 14:39:45 +0800

    另外一个测试的视频

--- Killer/Assets/YouYouTest/Scripts/Tutorial/TutorialControl.cs
+++ Killer/Assets/YouYouTest/Scripts/Tutorial/TutorialControl.cs
@@ -6,24 +6,18 @@ public class TutorialControl : MonoBehaviour
     public GameObject TutorialPanel;
     public VideoPlayer videoPlayer;
 
-    public void ShowTutorialPanel(GameObject gameObject, VideoClip videoClip)
+    public void ShowTutorialPanel(GameObject newPanel, VideoClip videoClip)
     {
         if (TutorialPanel != null)
         {
             TutorialPanel.SetActive(false);
-            TutorialPanel = gameObject;
-            TutorialPanel.SetActive(true);
-            videoPlayer.clip = videoClip;
-            videoPlayer.Stop();
-            videoPlayer.Play();
         }
-        else
-        {
-            TutorialPanel = gameObject;
-            TutorialPanel.SetActive(true);
-            videoPlayer.clip = videoClip;
-            videoPlayer.Stop();
-            videoPlayer.Play();
-        }
+
+        TutorialPanel = newPanel;
+        TutorialPanel.SetActive(true);
+
+        videoPlayer.clip = videoClip;
+        videoPlayer.Stop();
+        videoPlayer.Play();
     }
 }
